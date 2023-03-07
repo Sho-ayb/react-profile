a3e41828b65366fd4e4e0075239c244299fc5ba6
Sho.ayb <Sho.ayb@outlook.com>
2023-03-07 17:45:32 +0000

    components folder and basic component files created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing BrowserRouter, Link, Routes from react-router-dom
 
-function App() {
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+// importing all components
+
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="Projects" element={<Projects />} />
+            <Route path="Contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
