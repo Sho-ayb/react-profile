b9f3982c4d9889967bf8c7581ce294fd3a25b04e
Sho.ayb <Sho.ayb@outlook.com>
2023-03-11 12:02:03 +0000

    example cards are responsive

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,34 +3,115 @@ import Container from "./Container";
 import Row from "./Row";
 import Footer from "./Footer";
 
-import javascriptImage from "../Images/javascript-image.png";
+// import javascriptImage from "../Images/javascript-image.png";
 
 const Projects = () => {
   return (
     <>
       <Section className="section__projects">
         <Container className="container">
-          <Row className="align-items-md-stretch">
-            <h2 className="text-center">Projects</h2>
-            <Container className="container mt-3 mb-3">
-              <Row>
-                <div className="col">
-                  <div className="card h-100">
-                    <img
-                      src={javascriptImage}
-                      className="card-img-top"
-                      alt="card-image"
-                    />
-                    <div className="card-body align-self-end">
-                      <p className="card-text">
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
-                      </p>
-                    </div>
-                  </div>
+          <h2 className="text-center p-5">Projects</h2>
+          <Row className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+            <div className="col">
+              <div className="card w-auto h-100">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp"
+                  className="card-img-top"
+                  alt="Hollywood Sign on The Hill"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
                 </div>
-              </Row>
-            </Container>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card w-auto h-100">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp"
+                  className="card-img-top"
+                  alt="Palm Springs Road"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card w-auto h-100">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp"
+                  className="card-img-top"
+                  alt="Los Angeles Skyscrapers"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card w-auto h-100">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp"
+                  className="card-img-top"
+                  alt="Skyscrapers"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card w-auto h-100">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/new/standard/city/046.webp"
+                  className="card-img-top"
+                  alt="Skyscrapers"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card w-auto h-100">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/new/standard/city/050.webp"
+                  className="card-img-top"
+                  alt="Skyscrapers"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </div>
+              </div>
+            </div>
           </Row>
         </Container>
       </Section>
