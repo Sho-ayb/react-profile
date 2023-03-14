b33607f54a80d607e46f70bcb3b3dfaec0e353b3
Sho.ayb <Sho.ayb@outlook.com>
2023-03-14 17:22:19 +0000

    Deploying to git pages

--- src/App.js
+++ src/App.js
@@ -9,30 +9,13 @@ import {
 // importing all components
 
 import Header from "./Components/Header";
-// import Nav from "./Components/Nav";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
 import Contact from "./Components/Contact";
 
-const App = () => {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Root />}>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/contact" element={<Contact />} />
-      </Route>
-    )
-  );
-
-  return <RouterProvider router={router} />;
-};
-
-const Root = () => {
+const RootLayout = () => {
   return (
     <>
       <Header />
@@ -42,6 +25,21 @@ const Root = () => {
       </div>
     </>
   );
+};
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/contact" element={<Contact />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
 };
 
 export default App;
